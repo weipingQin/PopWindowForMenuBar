3c7c0cae084e089c7741f4c61c627a3640c3f277
weipingQin <995308963@qq.com>
2015-01-28 10:52:32 +0800

    first commit

---
+++ src/com/qinweiping/popwindow/PopWindowForMenuBar.java
@@ -0,0 +1,69 @@
+package com.qinweiping.popwindow;
+
+import com.example.popwindowformenubar.R;
+import com.qinweiping.common.AppUtil;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+public class PopWindowForMenuBar extends Activity {
+
+	private ActionBar mActionBar;
+	protected LayoutInflater mLayoutInflater;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mLayoutInflater.from(this);
+		setContentView(R.layout.popwindowmenubar);
+	}
+
+	//
+	private void init(){
+
+	}
+
+	@SuppressLint("NewApi")
+	private void initActionBar(boolean isShow){
+		mActionBar = getActionBar();
+		if(!isShow){
+			mActionBar.hide();
+			return;
+		}
+		mActionBar.setDisplayHomeAsUpEnabled(false);
+		mActionBar.setDisplayShowCustomEnabled(true);
+		mActionBar.setDisplayShowHomeEnabled(false);
+		mActionBar.setDisplayShowTitleEnabled(true);
+		View view = mLayoutInflater.inflate(R.layout.actionbar, null);
+		TextView backView = (TextView)view.findViewById(R.id.actionbar_back);
+		TextView titleView = (TextView)view.findViewById(R.id.actionbar_title);
+		
+		private OnClickListener onClickListener = new OnClickListener(){
+
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				switch(arg0.getId()){
+				case R.id.actionbar_back:
+					finish();
+					break;
+				case R.id.actionbar_title:
+					if(AppUtil.isNotEmpty(title)){
+						
+					}
+					break;
+				}
+			}
+		};
+		
+		backView.setOnClickListener(onClickListener);
+		titleView.setOnClickListener(onClickListener);
+	}
+	
+}
