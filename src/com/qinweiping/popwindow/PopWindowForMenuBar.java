be28b75d218f942cd11d2450e6311e3cc8e96485
qinweiping <2405991483@qq.com>
2015-09-24 16:06:20 +0800

    项目目录结构修改，代码依赖库版本调整

--- src/com/qinweiping/popwindow/PopWindowForMenuBar.java
+++ src/com/qinweiping/popwindow/PopWindowForMenuBar.java
@@ -1,47 +1,50 @@
 package com.qinweiping.popwindow;
 
-import com.example.popwindowformenubar.R;
 import com.qinweiping.common.AppUtil;
-
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
-public class PopWindowForMenuBar extends Activity {
+@SuppressLint("NewApi")
+public class PopWindowForMenuBar extends AppCompatActivity {
 
-	private ActionBar mActionBar;
+	private android.support.v7.app.ActionBar mActionBar;
 	protected LayoutInflater mLayoutInflater;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mLayoutInflater.from(this);
-		setContentView(R.layout.popwindowmenubar);
-		init();
+		initView();
 	}
 
-	//
-	private void init(){
+	private void initView(){
+		mLayoutInflater = LayoutInflater.from(PopWindowForMenuBar.this);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		initActionBar("µ¯´°²Ëµ¥",true);
 	}
 
 	@SuppressLint("NewApi")
 	private void initActionBar(final String str,boolean isShow){
-		mActionBar = getActionBar();
-		if(!isShow){
-			mActionBar.hide();
-			return;
+		mActionBar =getSupportActionBar();
+		if(mActionBar != null){
+			if(!isShow){
+				mActionBar.hide();
+				return;
+			}
+			mActionBar.setDisplayHomeAsUpEnabled(false);
+			mActionBar.setDisplayShowCustomEnabled(true);
+			mActionBar.setDisplayShowHomeEnabled(false);
+			mActionBar.setDisplayShowTitleEnabled(true);
 		}
-		mActionBar.setDisplayHomeAsUpEnabled(false);
-		mActionBar.setDisplayShowCustomEnabled(true);
-		mActionBar.setDisplayShowHomeEnabled(false);
-		mActionBar.setDisplayShowTitleEnabled(true);
 		View view = mLayoutInflater.inflate(R.layout.actionbar, null);
+		setContentView(view);
 		TextView backView = (TextView)view.findViewById(R.id.actionbar_back);
 		TextView titleView = (TextView)view.findViewById(R.id.actionbar_title);
 		backView.setOnClickListener(new OnClickListener() {
@@ -59,6 +62,7 @@ public class PopWindowForMenuBar extends Activity {
 			}
 		});
 	}
+	
 	
 	private void initMenu(){
 		TextView btnCopy  = (TextView)findViewById(R.id.actionbar_menu_item1);
